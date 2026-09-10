5dfe17646ea384871c41121e5b09dbe9c45a36f6
Feng Ruohang <rh@vonng.com>
2026-09-10 13:36:13 +0800

    fix: use the translated log table label
    Signed-off-by: Feng Ruohang <rh@vonng.com>

--- web-app/src/screens/Console/Logs/ErrorLogs/ErrorLogs.tsx
+++ web-app/src/screens/Console/Logs/ErrorLogs/ErrorLogs.tsx
@@ -295,7 +295,7 @@ const ErrorLogs = () => {
               }}
             >
               <Box withBorders customBorderPadding={"0px"} useBackground>
-                <Table aria-label={t("Error Logs")}>
+                <Table aria-label={t("Logs")}>
                   <TableBody>
                     {filteredMessages.map((m) => {
                       return <LogLine log={m} />;
